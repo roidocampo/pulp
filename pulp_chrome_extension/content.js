3b219f61e6b7830ef840ba7aa1efb2add8fcbfbb
Roi Docampo <roidocampo@gmail.com>
2016-10-10 16:49:35 -0500

    Fix https problems in chrome extension

--- pulp_chrome_extension/content.js
+++ pulp_chrome_extension/content.js
@@ -4,7 +4,19 @@ console.log("ArXiv title:", citation_title);
 
 var search_query = encodeURIComponent(citation_title);
 
-$.getJSON("http://localhost:23232/search?" + search_query, function(data) {
+function get_JSON(json_url, callback) {
+    chrome.runtime.sendMessage({
+        method: 'GET',
+        action: 'xhttp',
+        url: json_url,
+        data: ''
+    }, function(responseText) {
+        var data = JSON.parse(responseText);
+        callback(data);
+    })
+}
+
+get_JSON("http://localhost:23232/search?" + search_query, function(data) {
     console.log("Pulp Search:");
     console.log(data);
     if ($.isArray(data) && data.length > 0) {
@@ -18,7 +30,7 @@ $.getJSON("http://localhost:23232/search?" + search_query, function(data) {
             ;
         $(".full-text ul").prepend( $(li_html) );
         $(".open_in_pulp").on("click", function (){
-            $.getJSON("http://localhost:23232/open?" + file, function(data) {
+            get_JSON("http://localhost:23232/open?" + file, function(data) {
                 console.log("Opened: " + file);
             });
         });
